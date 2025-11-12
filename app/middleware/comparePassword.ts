27158608f76ac90319b2d1655239b3137a240fc8
espensolhaug76 <153593072+espensolhaug76@users.noreply.github.com>
2025-11-12 10:36:10 +0100

    Change bcrypt import to use namespace import

--- app/middleware/comparePassword.ts
+++ app/middleware/comparePassword.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt"
+import * as bcrypt from "bcrypt"
 import HttpStatusCode from "http-status-codes"
 
 import { Request, Response, NextFunction } from "../interfaces/express.interface"
